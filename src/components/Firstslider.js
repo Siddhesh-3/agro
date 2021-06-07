1ece1c1c31ccc038a42b98c06f6ca058009a506f
Siddhesh-3 <siddhesh.gaykar017@gmail.com>
2021-06-07 21:32:02 +0530

    no change

---
+++ src/components/Firstslider.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const tutorialSteps = [
+  {
+    label: 'Serving farmar all over the world',
+    imgPath:
+      '	https://ariesagro.com/wp-content/uploads/2019/01/Crop2.jpg',
+    
+  },
+  {
+    label: 'Manufacturing Quality of Products',
+    imgPath:
+      'https://ariesagro.com/wp-content/uploads/2019/01/slide1.jpg',
+    
+  },
+  {
+    label: 'Award Winning Marketing',
+    imgPath:
+      'https://ariesagro.com/wp-content/uploads/2019/01/slide2.jpg',
+     
+  }
+
+];
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: '100%',
+    flexGrow: 1,
+  },
+  header: {
+    position: 'absolute',
+    bottom: '20px',
+    right: '20px',
+    backgroundcolor: 'black',
+    color: 'white',
+    paddingleft: '20px',
+    paddingright: '20px'
+    
+  },
+  img: {
+    height: '100%',
+    display: 'block',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    width: '100%',
+    
+  },
+}));
+
+function Firstslider() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = tutorialSteps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+          
+        {tutorialSteps.map((step, index) => (
+          <div key={step.label}>  
+            {/* img */}
+            {Math.abs(activeStep - index) <= 2 ? (
+                <div className="container" >
+                <img className={classes.img} src={step.imgPath} alt={step.label} />
+                <div class="text-block">
+                <h1 className="imgtext">{step.label}</h1>
+                </div>
+                
+                </div>
+            ) : null}
+
+
+          </div>
+ 
+        ))}
+      </AutoPlaySwipeableViews>
+      
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            
+          </Button>
+        }
+      />
+    </div>
+  );
+}
+
+export default Firstslider;
--- src/components/Landing1.js
+++ src/components/Landing1.js
@@ -2,12 +2,14 @@ import { Button, Grid, Hidden } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react'
 import "../App.css"
+import Firstslider from './Firstslider';
+import Landing5 from "./Landing5"
 const Landing1 = () => {
     return (
         <div> <br></br>
             <Grid lg={12} xs={12} sm={12} container direction="row">
                 <Grid container item xs={5} spacing={1}>
-                    <img src="logo.png" style={{ height: "60%", width: "60%", marginLeft: "10%",padding:"4%" }} alt="logo" />
+                    <img src="logo.png" style={{ height: "60%", width: "60%", marginLeft: "10%", padding: "4%" }} alt="logo" />
                 </Grid>
 
                 <Grid container item xs={7} spacing={2} style={{ marginTop: "3%" }}>
@@ -28,8 +30,13 @@ const Landing1 = () => {
                             }} />
                     </Hidden>
                 </Grid>
+                
+                <Grid container item xs={12} spacing={0}>
+                    <div>
+                   <Firstslider/>
+                    </div>
 
-
+                </Grid>              
             </Grid>
         </div>
     )
