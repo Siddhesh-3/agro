69fa370b9513151171b0c210936b2bd5125f900a
Siddhesh-3 <siddhesh.gaykar017@gmail.com>
2021-06-07 22:20:13 +0530

    Working Menu for mobille view

--- src/components/Firstslider.js
+++ src/components/Firstslider.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
--- src/components/Landing1.js
+++ src/components/Landing1.js
@@ -3,8 +3,113 @@ import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react'
 import "../App.css"
 import Firstslider from './Firstslider';
-import Landing5 from "./Landing5"
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
 const Landing1 = () => {
+
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Home'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <List>
+                {['Product&Services'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <List>
+                {['Blank'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+                        <List>
+                {['Crops'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <List>
+                {['About Us'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <List>
+                {['More'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            
+            
+            <Divider />
+            {/* <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List> */}
+        </div>
+    );
+
     return (
         <div> <br></br>
             <Grid lg={12} xs={12} sm={12} container direction="row">
@@ -22,20 +127,29 @@ const Landing1 = () => {
                         <h4 className="menu" style={{ marginLeft: "8%" }}>More</h4>
                     </Hidden>
                     <Hidden smUp>
-                        <MenuIcon name="md-close"
-                            style={{
-                                position: 'absolute',
-                                right: 40,
-                                top: 20,
-                            }} />
+                        {['right'].map((anchor) => (
+                            <React.Fragment key={anchor}>
+                                <MenuIcon name="md-close"
+                                    style={{
+                                        position: 'absolute',
+                                        right: 40,
+                                        top: 20,
+                                    }}
+                                    onClick={toggleDrawer(anchor, true)}
+                                />
+                                <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                                    {list(anchor)}
+                                </Drawer>
+                            </React.Fragment>
+                        ))}
                     </Hidden>
                 </Grid>
 
                 <Grid container item xs={12} spacing={0}>
                     <div>
-                   <Firstslider/>
+                        <Firstslider />
                     </div>
-                </Grid>              
+                </Grid>
             </Grid>
         </div>
     )
